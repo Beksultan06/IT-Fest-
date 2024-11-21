247e208357a09da77bf6cd42fdbc269f6b140001
didiotghima <nurlanuuulubeksultan@gmail.com>
2024-11-21 20:22:09 +0600

    commit

--- app/base/models.py
+++ app/base/models.py
@@ -1,3 +1,27 @@
 from django.db import models
-
+from ckeditor.fields import RichTextField
 # Create your models here.
+class Services(models.Model):
+    name = models.CharField(
+        max_length=155,
+        verbose_name='Имя'
+    )
+    phone = models.CharField(
+        max_length=50
+    )
+    email = models.EmailField()
+    zip_code = models.CharField(
+        max_length=155
+    )
+    services = models.CharField(
+        max_length=155
+    )
+    descriptions = models.CharField(
+        max_length=155
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Обратный связ услуг'
--- app/base/views.py
+++ app/base/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Services
+from django.contrib import messages
 
-# Create your views here.
+def index(request):
+    if request.method == "POST":
+        name = request.POST.get("name")
+        phone = request.POST.get("phone")
+        email = request.POST.get("email")
+        zip_code = request.POST.get("zipcode")
+        services = request.POST.get("services")
+        descriptions = request.POST.get("description")
+
+        # Сохранение данных в модель
+        service = Services(
+            name=name,
+            phone=phone,
+            email=email,
+            zip_code=zip_code,
+            services=services,
+            descriptions=descriptions
+        )
+        service.save()
+        messages.success(request, "Ваш запрос успешно отправлен!")
+        return redirect("index")
+
+    return render(request, "index.html")
